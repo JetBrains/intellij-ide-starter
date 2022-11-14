add33960f5f5f418460ec89674d4d69f0ba2c252
Anna Koehler <anna.marchenko@jetbrains.com>
2022-11-14 22:38:39 +0100

    Integration tests: add forgotten file
    GitOrigin-RevId: 5b6322ae7d5edc8224ac41781ebf6974ea0803f6

--- intellij.tools.ide.starter.tests/testSrc/com/intellij/ide/starter/tests/examples/data/TestCases.kt
+++ intellij.tools.ide.starter.tests/testSrc/com/intellij/ide/starter/tests/examples/data/TestCases.kt
@@ -6,6 +6,7 @@ package com.intellij.ide.starter.tests.examples.data
 object TestCases {
   /** IntelliJ Community cases */
   val IC = IdeaCommunityCases
+  val IU = IdeaUltimateCases
 
   //val DB = DataGripCases
 
