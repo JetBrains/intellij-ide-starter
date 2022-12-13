852fe8d89bcaa630bc58743e28342479ee2fea93
Anna Koehler <anna.marchenko@jetbrains.com>
2022-12-13 20:20:24 +0100

    Integration tests: use the project Community of corresponded version in tests
    GitOrigin-RevId: a67cfdc7c4a61942d273e63e8974859915b79329

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/utils/Git.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/utils/Git.kt
@@ -12,6 +12,7 @@ import kotlin.time.Duration.Companion.minutes
 object Git {
   val branch by lazy { getShortBranchName() }
   val localBranch by lazy { getLocalGitBranch() }
+  val getDefaultBranch by lazy { localBranch.substringBefore(".")}
 
   @Throws(IOException::class, InterruptedException::class)
   private fun getLocalGitBranch(): String {
