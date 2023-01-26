5482e1aab5d8682525909e75c7326f7a0d61c8d2
Anna Koehler <anna.marchenko@jetbrains.com>
2023-01-26 23:52:23 +0100

    Startup tests: run tests for startup twice
    GitOrigin-RevId: bb6b41a8528721237b6892a50f6c6c9c231ff9a7

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/models/IDEStartupReports.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/models/IDEStartupReports.kt
@@ -3,9 +3,12 @@ package com.intellij.ide.starter.models
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ObjectNode
 import java.nio.file.Path
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class IDEStartupReports(startupReportsDir: Path) {
-  val statsJSON: Path = startupReportsDir.resolve("startup-stats.json")
+  val time: String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+  val statsJSON: Path = startupReportsDir.resolve("startup-stats-$time.json")
 
   val statsObject: ObjectNode
     get() = ObjectMapper().readTree(statsJSON.toFile()) as ObjectNode
