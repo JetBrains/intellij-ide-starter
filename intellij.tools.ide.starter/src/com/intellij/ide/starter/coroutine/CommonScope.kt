51d099817c1f8b09eca59bea23f05fc8e3b96da6
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-01-27 12:41:28 +0200

    [ide starter] comments about coroutine scopes
    GitOrigin-RevId: ff9526ee4850c62caca66a85804267e1b5412811

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/coroutine/CommonScope.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/coroutine/CommonScope.kt
@@ -5,5 +5,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 
+/**
+ * Unhandled exceptions in the tree of child coroutines will not affect any other coroutines (parent included).
+ * Usually that what you need.
+ */
 val supervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+/**
+ * In case of unhandled exception in child coroutines all the coroutines tree (parents and other branches) will be cancelled.
+ * In most scenarious you don't need that behaviour.
+ */
 val simpleScope = CoroutineScope(Job() + Dispatchers.IO)
