31244778f1e519550dee1e186379f418d3c9f493
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-02-12 13:01:41 +0200

    [performance-tests] AT-26 add tests to publisher and error reader
    GitOrigin-RevId: a98234570165c26231fcb142a71e4f4f2ca16e78

--- intellij.tools.ide.starter.tests/testSrc/com/intellij/ide/starter/tests/unit/ReportPublisherTest.kt
+++ intellij.tools.ide.starter.tests/testSrc/com/intellij/ide/starter/tests/unit/ReportPublisherTest.kt
@@ -12,6 +12,8 @@ import com.intellij.ide.starter.path.IDEDataPaths
 import com.intellij.ide.starter.report.publisher.ReportPublisher
 import com.intellij.ide.starter.runner.IDERunContext
 import com.intellij.ide.starter.tests.examples.data.TestCases
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.kodein.di.direct
@@ -33,16 +35,21 @@ class ReportPublisherTest {
   @Mock
   lateinit var ideStartResult: IDEStartResult
 
-  @Test
-  fun `test that report publishers were invoked successfully`() {
-    //GIVEN
-    //TODO(Find a way to mock on stage of init of di, due to it little bit dirty approach to put them directly in IDETestContext publishers)
-    val publishers: List<ReportPublisher> = di.direct.instance()
-    val publisherSpy = spy(publishers[0])
-    val commandChain = CommandChain()
-    val patchVMOptions: VMOptions.() -> VMOptions = { this }
+  @Mock
+  lateinit var context: IDETestContext
+  private val commandChain = CommandChain()
+  private val patchVMOptions: VMOptions.() -> VMOptions = { this }
 
-    val ideTestContext = IDETestContext(
+  private lateinit var publishers: List<ReportPublisher>
+  private lateinit var publisherSpy: ReportPublisher
+  private lateinit var ideTestContext: IDETestContext
+
+  @BeforeEach
+  fun before() {
+    //TODO(Find a way to mock on stage of init of di, due to it little bit dirty approach to put them directly in IDETestContext publishers)
+    publishers = di.direct.instance()
+    publisherSpy = spy(publishers[0])
+    ideTestContext = IDETestContext(
       ideDataPaths,
       installedIDE,
       TestCases.IC.GradleJitPackSimple,
@@ -53,7 +60,11 @@ class ReportPublisherTest {
       publishers = listOf(publisherSpy),
       isReportPublishingEnabled = true
     )
+  }
 
+  @Test
+  fun `test that report publishers were invoked successfully`() {
+    //GIVEN
     val ideRunContext = IDERunContext(ideTestContext)
     val spyIdeRunContext = spy(ideRunContext)
     val spyTestContext = spy(ideTestContext)
@@ -61,11 +72,32 @@ class ReportPublisherTest {
     //WHEN
     doReturn(spyIdeRunContext).`when`(spyTestContext).runContext(commands = commandChain, patchVMOptions = patchVMOptions)
     doReturn(ideStartResult).`when`(spyIdeRunContext).runIDE()
+    doReturn(context).`when`(spyIdeRunContext).testContext
 
     //THEN
     spyTestContext.runIDE(commands = commandChain, patchVMOptions = patchVMOptions)
-
     //ASSERT
     verify(publisherSpy, times(1)).publishResult(ideStartResult)
+    verify(publisherSpy, times(1)).publishAnyway(context)
+  }
+
+  @Test
+  fun `test that report publisher were fail due exception and only publishAnyway report`() {
+    //GIVEN
+    val ideRunContext = IDERunContext(ideTestContext)
+    val spyIdeRunContext = spy(ideRunContext)
+    val spyTestContext = spy(ideTestContext)
+
+    //WHEN
+    doReturn(spyIdeRunContext).`when`(spyTestContext).runContext(commands = commandChain, patchVMOptions = patchVMOptions)
+    doThrow(IllegalArgumentException::class.java).`when`(spyIdeRunContext).runIDE()
+    doReturn(context).`when`(spyIdeRunContext).testContext
+    //THEN
+    Assertions.assertThrows(IllegalArgumentException::class.java) {
+      spyTestContext.runIDE(commands = commandChain, patchVMOptions = patchVMOptions)
+    }
+    //ASSERT
+    verify(publisherSpy, times(0)).publishResult(ideStartResult)
+    verify(publisherSpy, times(1)).publishAnyway(context)
   }
 }
