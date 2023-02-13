cb6b78ea3d6a0a081d1e9b95744d74d6dd17072c
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-02-13 15:59:01 +0200

    [performance-tests] add j2k test on kotlin language server project
    GitOrigin-RevId: e028624847ab2036ec1cce4f434d0b5348f95036

--- intellij.tools.ide.performanceTesting.commands/src/com/jetbrains/performancePlugin/commands/chain/generalCommandChain.kt
+++ intellij.tools.ide.performanceTesting.commands/src/com/jetbrains/performancePlugin/commands/chain/generalCommandChain.kt
@@ -581,10 +581,6 @@ fun <T : CommandChain> T.assertOpenedFileInRoot(): T {
   addCommand("${CMD_PREFIX}assertOpenedFileInRoot")
   return this
 }
-fun <T : CommandChain> T.assertOpenedKotlinFileInRoot(): T {
-  addCommand("${CMD_PREFIX}assertOpenedKotlinFileInRoot")
-  return this
-}
 
 const val IMPORT_GRADLE_PROJECT_CMD_PREFIX = "${CMD_PREFIX}importGradleProject"
 
