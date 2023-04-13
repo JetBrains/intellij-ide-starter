4684a2c4da1f8756e2c321dce6c1cc5859fdf37c
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-04-13 17:22:33 +0200

    Improve test for scalability
    Provide a way to set an existing project/plugins/config and output the results in CSV format
    
    GitOrigin-RevId: 15b0f74d393d2e76c3fbf5a6b5e3dbaffd9c9e8a

--- intellij.tools.ide.starter.examples/testSrc/com/intellij/ide/starter/examples/indexing/ScalabilityTest.kt
+++ intellij.tools.ide.starter.examples/testSrc/com/intellij/ide/starter/examples/indexing/ScalabilityTest.kt
@@ -1,23 +1,25 @@
 package com.intellij.ide.starter.examples.indexing
 
 import com.intellij.ide.starter.di.di
-import com.intellij.ide.starter.examples.data.TestCases
+import com.intellij.ide.starter.ide.IDETestContext
+import com.intellij.ide.starter.ide.IdeProductProvider
 import com.intellij.ide.starter.ide.command.CommandChain
 import com.intellij.ide.starter.junit5.JUnit5StarterAssistant
 import com.intellij.ide.starter.junit5.hyphenateWithClass
+import com.intellij.ide.starter.models.TestCase
+import com.intellij.ide.starter.project.LocalProjectInfo
 import com.intellij.ide.starter.report.publisher.ReportPublisher
 import com.intellij.ide.starter.report.publisher.impl.ConsoleTestResultPublisher
 import com.intellij.ide.starter.runner.TestContainerImpl
 import com.intellij.metricsCollector.metrics.extractIndexingMetrics
-import com.intellij.metricsCollector.publishing.publishIndexingMetrics
 import com.jetbrains.performancePlugin.commands.chain.exitApp
 import com.jetbrains.performancePlugin.commands.chain.waitForSmartMode
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
 import org.kodein.di.DI
 import org.kodein.di.bindSingleton
+import java.nio.file.Paths
 
 @ExtendWith(JUnit5StarterAssistant::class)
 class ScalabilityTest {
@@ -34,16 +36,44 @@ class ScalabilityTest {
     }
   }
 
-  @ParameterizedTest
-  @ValueSource(ints = [1, 2, 4, 8, 16, 32, 64])
-  fun indexingGradleJitPackSimpleProject(processorCount: Int) {
-    val context = context
-      .initializeTestContext("${testInfo.hyphenateWithClass()}_$processorCount", TestCases.IC.GradleJitPackSimple)
-      .setActiveProcessorCount(processorCount)
+  private fun IDETestContext.setActiveProcessorCount(count: Int): IDETestContext =
+    this.addVMOptionsPatch {
+      this.addLine("-XX:ActiveProcessorCount=$count", "-XX:ActiveProcessorCount")
+    }
 
-    val commands = CommandChain().waitForSmartMode().exitApp()
+  @Test
+  fun scalabilityOfIndexingTest() {
+    //CONFIGURATION
+    //provide path to your local project
+    val testCase = TestCase(IdeProductProvider.IU, LocalProjectInfo(Paths.get("/Users/maxim.kolmakov/IdeaProjects/coroutines")))
+    //provide the required release
+      .useRelease("2023.1")
 
-    val result = context.runIDE(commands = commands)
-    extractIndexingMetrics(result).publishIndexingMetrics()
+    //provide path to config with valid license
+    val config = Paths.get("/Users/maxim.kolmakov/Library/Application Support/JetBrains/IntelliJIdea2023.1")
+    //provide path to plugins if needed
+    val plugins = Paths.get("/Users/maxim.kolmakov/Library/Application Support/JetBrains/Toolbox/apps/IDEA-U/ch-1/231.8109.175/IntelliJ IDEA 2023.1 EAP.app.plugins")
+
+    val processorCounts = listOf(1, 2, 4, 8, 16, 32, 64)
+    val results = mutableMapOf<Int, List<Long>>()
+    for (processorCount in processorCounts) {
+      val context = context
+        .initializeTestContext("${testInfo.hyphenateWithClass()}_$processorCount", testCase)
+        .copyExistingConfig(config)
+        .copyExistingPlugins(plugins)
+        .setActiveProcessorCount(processorCount)
+
+      val commands = CommandChain().waitForSmartMode().exitApp()
+
+      val result = context.runIDE(commands = commands)
+      val indexingMetrics = extractIndexingMetrics(result)
+      results[processorCount] = listOf(indexingMetrics.totalIndexingTime, indexingMetrics.totalScanFilesTime)
+    }
+    println("\n###RESULTS###")
+    println("Number of processors, Total Indexing Time, Total Scanning Time")
+    for (result in results) {
+      println(result.key.toString() + "," + result.value.joinToString(","))
+    }
+    println("######\n")
   }
 }
