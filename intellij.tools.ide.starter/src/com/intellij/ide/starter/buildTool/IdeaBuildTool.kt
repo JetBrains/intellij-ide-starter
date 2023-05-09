3f0d9ac797f3999c63601ded4669de4af76f5a5b
Anna Koehler <anna.marchenko@jetbrains.com>
2023-05-09 08:29:56 +0200

    Performance tests: fix setting compiler options in performance tests
    GitOrigin-RevId: e59df8fbf1fdb593e6f31864e3274d7092125325

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/buildTool/IdeaBuildTool.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/buildTool/IdeaBuildTool.kt
@@ -8,6 +8,7 @@ import javax.xml.xpath.XPath
 import javax.xml.xpath.XPathConstants
 import javax.xml.xpath.XPathFactory
 import kotlin.io.path.notExists
+import kotlin.io.path.readText
 
 
 class IdeaBuildTool(testContext: IDETestContext) : BuildTool(BuildToolType.IDEA, testContext) {
@@ -19,14 +20,37 @@ class IdeaBuildTool(testContext: IDETestContext) : BuildTool(BuildToolType.IDEA,
 
   fun setBuildProcessHeapSize(heapSizeMb: Int): IdeaBuildTool {
     if (compilerXmlPath.notExists()) return this
+    val content = compilerXmlPath.readText()
+    if (content.contains("option name=\"BUILD_PROCESS_HEAP_SIZE\" value=\"$heapSizeMb\"")) return this
 
     val xmlDoc = XmlBuilder.parse(compilerXmlPath)
     xmlDoc.documentElement.normalize()
-
     val xp: XPath = XPathFactory.newInstance().newXPath()
-    val node =  xp.evaluate("//component/option[@name='BUILD_PROCESS_HEAP_SIZE']", xmlDoc, XPathConstants.NODE) as Element
-    node.removeAttribute("value")
-    node.setAttribute("value", "$heapSizeMb")
+
+    if (content.contains("CompilerConfiguration")) {
+
+      if (compilerXmlPath.readText().contains("BUILD_PROCESS_HEAP_SIZE")) {
+        val node = xp.evaluate("//component/option[@name='BUILD_PROCESS_HEAP_SIZE']", xmlDoc, XPathConstants.NODE) as Element
+        node.removeAttribute("value")
+        node.setAttribute("value", "$heapSizeMb")
+      }
+      else {
+        val componentNode = xp.evaluate("//component[@name='CompilerConfiguration']", xmlDoc, XPathConstants.NODE) as Element
+        val optionElement = xmlDoc.createElement("option")
+        optionElement.setAttribute("name", "BUILD_PROCESS_HEAP_SIZE")
+        optionElement.setAttribute("value", "$heapSizeMb")
+        componentNode.appendChild(optionElement)
+      }
+    }
+    else {
+      val firstNode = xmlDoc.firstChild
+      val componentElement = xmlDoc.createElement("component")
+      componentElement.setAttribute("name", "CompilerConfiguration")
+      val optionElement = xmlDoc.createElement("option")
+      optionElement.setAttribute("name", "BUILD_PROCESS_HEAP_SIZE")
+      optionElement.setAttribute("value", "$heapSizeMb")
+      firstNode.appendChild(componentElement).appendChild(optionElement)
+    }
     XmlBuilder.writeDocument(xmlDoc, compilerXmlPath)
 
     return this
@@ -34,19 +58,44 @@ class IdeaBuildTool(testContext: IDETestContext) : BuildTool(BuildToolType.IDEA,
 
   fun addBuildVmOption(key: String, value: String): IdeaBuildTool {
     if (compilerXmlPath.notExists()) return this
+    val content = compilerXmlPath.readText()
+    if (content.contains("-D$key=$value")) return this
 
     val xmlDoc = XmlBuilder.parse(compilerXmlPath)
     xmlDoc.documentElement.normalize()
-
     val xp: XPath = XPathFactory.newInstance().newXPath()
-    val node =  xp.evaluate("//component/option[@name='BUILD_PROCESS_ADDITIONAL_VM_OPTIONS']", xmlDoc, XPathConstants.NODE) as Element
-    val oldValue = node.getAttribute("value")
 
-    if (oldValue.contains(value)) return this
+    if (content.contains("CompilerConfiguration")) {
 
-    val newValue = "$oldValue -D$key=$value"
-    node.removeAttribute("value")
-    node.setAttribute("value", newValue)
+      if (compilerXmlPath.readText().contains("BUILD_PROCESS_ADDITIONAL_VM_OPTIONS")) {
+
+        val optionNode = xp.evaluate("//component/option[@name='BUILD_PROCESS_ADDITIONAL_VM_OPTIONS']", xmlDoc,
+                                     XPathConstants.NODE) as Element
+        val oldValue = optionNode.getAttribute("value")
+
+        if (oldValue.contains(value)) return this
+
+        val newValue = "$oldValue -D$key=$value"
+        optionNode.removeAttribute("value")
+        optionNode.setAttribute("value", newValue)
+      }
+      else {
+        val componentNode = xp.evaluate("//component[@name='CompilerConfiguration']", xmlDoc, XPathConstants.NODE) as Element
+        val optionElement = xmlDoc.createElement("option")
+        optionElement.setAttribute("name", "BUILD_PROCESS_ADDITIONAL_VM_OPTIONS")
+        optionElement.setAttribute("value", "-D$key=$value")
+        componentNode.appendChild(optionElement)
+      }
+    }
+    else {
+      val firstNode = xmlDoc.firstChild
+      val componentElement = xmlDoc.createElement("component")
+      componentElement.setAttribute("name", "CompilerConfiguration")
+      val optionElement = xmlDoc.createElement("option")
+      optionElement.setAttribute("name", "BUILD_PROCESS_ADDITIONAL_VM_OPTIONS")
+      optionElement.setAttribute("value", "-D$key=$value")
+      firstNode.appendChild(componentElement).appendChild(optionElement)
+    }
     XmlBuilder.writeDocument(xmlDoc, compilerXmlPath)
 
     return this
