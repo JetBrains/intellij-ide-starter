413347b39131db0c483783cf33db663c550845b4
Anna Koehler <anna.marchenko@jetbrains.com>
2023-05-16 22:26:34 +0200

    Performance tests for incremental compilation have been added + small refactoring
    GitOrigin-RevId: 186196fe3d297fc996ff63c8f271c416102ed56a

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/process/exec/execUtil.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/process/exec/execUtil.kt
@@ -1,9 +1,7 @@
 package com.intellij.ide.starter.process.exec
 
-import com.intellij.ide.starter.system.SystemInfo
 import com.intellij.ide.starter.utils.logOutput
 import java.nio.file.Path
-import kotlin.io.path.div
 import kotlin.time.Duration.Companion.minutes
 
 
@@ -25,34 +23,4 @@ fun executeScript(fileNameToExecute: String, projectDirPath: Path) {
 
   logOutput("Stdout of command execution $commit")
   logOutput("Stderr of command execution $error")
-}
-
-fun execGradlew(pathToProject: Path, args: List<String>) {
-  val stdout = ExecOutputRedirect.ToString()
-  val stderr = ExecOutputRedirect.ToString()
-
-  val command = when (SystemInfo.isWindows) {
-    true -> (pathToProject / "gradlew.bat").toString()
-    false -> "./gradlew"
-  }
-
-  if (!SystemInfo.isWindows) {
-    ProcessExecutor(
-      presentableName = "chmod gradlew",
-      workDir = pathToProject,
-      timeout = 1.minutes,
-      args = listOf("chmod", "+x", "gradlew"),
-      stdoutRedirect = stdout,
-      stderrRedirect = stderr
-    ).start()
-  }
-
-  ProcessExecutor(
-    presentableName = "Gradle Format",
-    workDir = pathToProject,
-    timeout = 1.minutes,
-    args = listOf(command) + args,
-    stdoutRedirect = stdout,
-    stderrRedirect = stderr
-  ).start()
 }
