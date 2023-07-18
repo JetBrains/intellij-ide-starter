8b0b0327adb0036298f75aa4271cf55aeebe9afe
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-07-18 16:50:22 +0200

    IJ-CR-111034 Save allure report by default under testHomePath and upload it even if test was not run
    + minor fixes after review
    
    GitOrigin-RevId: 4ab4b398f668a96a9227bb43791ebb09f5c122ed

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/report/AllureWriter.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/report/AllureWriter.kt
@@ -1,15 +1,19 @@
 package com.intellij.ide.starter.report
 
+import com.intellij.ide.starter.di.di
+import com.intellij.ide.starter.path.GlobalPaths
 import io.qameta.allure.AllureResultsWriter
 import io.qameta.allure.FileSystemResultsWriter
 import io.qameta.allure.model.TestResult
 import io.qameta.allure.model.TestResultContainer
+import org.kodein.di.direct
+import org.kodein.di.instance
 import java.io.InputStream
 import java.nio.file.Path
-import java.nio.file.Paths
+import kotlin.io.path.div
 
 object AllureWriter: AllureResultsWriter {
-  var path: Path = Paths.get("")
+  var path: Path = di.direct.instance<GlobalPaths>().testHomePath / "tmp" / "allure"
   override fun write(testResult: TestResult?) {
     FileSystemResultsWriter(path).write(testResult)
   }
