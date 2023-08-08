6197264f6e982db2b8a9ebacb01f079067eda74c
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2023-08-08 13:39:48 +0300

    [starter] fix report error method to avoid bad formatting of string
    GitOrigin-RevId: 08d2895b5271942b559536104107f363660ec624

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/ci/teamcity/TeamCityCIServer.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/ci/teamcity/TeamCityCIServer.kt
@@ -31,12 +31,12 @@ open class TeamCityCIServer(
 
     val generifiedTestName = testName.processStringForTC()
 
-    logOutput(String.format("##teamcity[testStarted name='%s' flowId='%s' nodeId='${generifiedTestName}' parentNodeId='0']", generifiedTestName, flowId))
+    logOutput(String.format("##teamcity[testStarted name='%s' flowId='%s' nodeId='%s' parentNodeId='0']", generifiedTestName, flowId, generifiedTestName))
     logOutput(String.format(
-      "##teamcity[testFailed name='%s' message='%s' details='%s' flowId='%s' nodeId='${generifiedTestName}' parentNodeId='0']",
-      generifiedTestName, message.processStringForTC(), details.processStringForTC(), flowId
+      "##teamcity[testFailed name='%s' message='%s' details='%s' flowId='%s' nodeId='%s' parentNodeId='0']",
+      generifiedTestName, message.processStringForTC(), details.processStringForTC(), flowId, generifiedTestName
     ))
-    logOutput(String.format("##teamcity[testFinished name='%s' flowId='%s' nodeId='${generifiedTestName}' parentNodeId='0']", generifiedTestName, flowId))
+    logOutput(String.format("##teamcity[testFinished name='%s' flowId='%s' nodeId='%s' parentNodeId='0']", generifiedTestName, flowId, generifiedTestName))
   }
 
   override fun ignoreTestFailure(testName: String, message: String, details: String) {
