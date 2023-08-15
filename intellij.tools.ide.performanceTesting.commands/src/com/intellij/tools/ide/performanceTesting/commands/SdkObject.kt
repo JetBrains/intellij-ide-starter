3af7691050458f08d324de43ad6096bd8f721e0c
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-08-15 17:07:48 +0200

    [testing] Relayout performanceTesting starter and commands modules
    GitOrigin-RevId: d236ad263edaba1158b9b997886d6fe8ad099f40

--- intellij.tools.ide.performanceTesting.commands/src/com/intellij/tools/ide/performanceTesting/commands/SdkObject.kt
+++
@@ -1,9 +0,0 @@
-package com.intellij.tools.ide.performanceTesting.commands
-
-import java.nio.file.Path
-
-data class SdkObject(
-  val sdkName: String,
-  val sdkType: String,
-  val sdkPath: Path,
-)
