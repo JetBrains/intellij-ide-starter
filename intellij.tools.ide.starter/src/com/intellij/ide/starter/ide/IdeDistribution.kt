4b0727b355854bec089b8034fbb471ef79487227
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-09-28 18:10:12 +0200

    IJ-CR-115747 Refactor JBRDownloader and move JBRVersion class
    The "JBRDownloader" was refactored from a class with a companion object to a singleton object for simplicity and readability. The JBRVersion class, has been moved from "IdeDistribution.kt" to "JBRDownloader.kt". This centralizes the JBR related logic within the "JBRDownloader" object .
    
    GitOrigin-RevId: 489b1dba53cd605e30298a7b128720b6ce5e46e0

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/IdeDistribution.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/IdeDistribution.kt
@@ -2,9 +2,6 @@ package com.intellij.ide.starter.ide
 
 import java.nio.file.Path
 
-
-data class JBRVersion(val majorVersion: String, val buildNumber: String)
-
 abstract class IdeDistribution {
   abstract fun installIde(unpackDir: Path, executableFileName: String): InstalledIde
 }
--- intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/JBRDownloader.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/JBRDownloader.kt
@@ -9,101 +9,100 @@ import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.time.Duration.Companion.minutes
 
-class JBRDownloader {
+object JBRDownloader {
 
-  companion object {
+  data class JBRVersion(val majorVersion: String, val buildNumber: String)
 
-    private fun getJBRVersionFromBuild(jbrFullVersion: String): JBRVersion {
-      val jbrVersion = jbrFullVersion.split(".")[0].toInt()
-      var majorVersion = jbrFullVersion.split("+").firstOrNull()
-      if (jbrVersion < 17) {
-        majorVersion = majorVersion?.replace(".", "_")
-      }
-      requireNotNull(majorVersion) {
-        { "majorVersion is: $majorVersion" }
-      }
-      val buildNumber = jbrFullVersion.split("-b").drop(1).singleOrNull()
-      requireNotNull(buildNumber) {
-        { "buildNumber is: $buildNumber" }
-      }
-      logOutput("Detected JBR version $jbrFullVersion with parts: $majorVersion and build $buildNumber")
-      return JBRVersion(majorVersion, buildNumber)
+  private fun getJBRVersionFromBuild(jbrFullVersion: String): JBRVersion {
+    val jbrVersion = jbrFullVersion.split(".")[0].toInt()
+    var majorVersion = jbrFullVersion.split("+").firstOrNull()
+    if (jbrVersion < 17) {
+      majorVersion = majorVersion?.replace(".", "_")
+    }
+    requireNotNull(majorVersion) {
+      { "majorVersion is: $majorVersion" }
+    }
+    val buildNumber = jbrFullVersion.split("-b").drop(1).singleOrNull()
+    requireNotNull(buildNumber) {
+      { "buildNumber is: $buildNumber" }
+    }
+    logOutput("Detected JBR version $jbrFullVersion with parts: $majorVersion and build $buildNumber")
+    return JBRVersion(majorVersion, buildNumber)
+  }
+
+  private fun getJBRVersionFromSources(jbrFullVersion: String): JBRVersion {
+    val jbrVersion = jbrFullVersion.split(".")[0].toInt()
+    var majorVersion = jbrFullVersion.split("b").firstOrNull()
+    if (jbrVersion < 17) {
+      majorVersion = majorVersion?.replace(".", "_")
+    }
+    requireNotNull(majorVersion) {
+      { "majorVersion is: $majorVersion" }
+    }
+    val buildNumber = jbrFullVersion.split("b").drop(1).singleOrNull()
+    requireNotNull(buildNumber) {
+      { "buildNumber is: $buildNumber" }
+    }
+    logOutput("Detected JBR version $jbrFullVersion with parts: $majorVersion and build $buildNumber")
+    return JBRVersion(majorVersion, buildNumber)
+  }
+
+  fun downloadAndUnpackJbrFromBuildIfNeeded(jbrFullVersion: String): Path {
+    return downloadAndUnpackJbrIfNeeded(getJBRVersionFromBuild(jbrFullVersion))
+  }
+
+  fun downloadAndUnpackJbrFromSourcesIfNeeded(jbrFullVersion: String): Path {
+    return downloadAndUnpackJbrIfNeeded(getJBRVersionFromSources(jbrFullVersion))
+  }
+
+  private fun downloadAndUnpackJbrIfNeeded(jbrVersion: JBRVersion): Path {
+    val (majorVersion, buildNumber) = listOf(jbrVersion.majorVersion, jbrVersion.buildNumber)
+
+    val os = when {
+      SystemInfo.isWindows -> "windows"
+      SystemInfo.isLinux -> "linux"
+      SystemInfo.isMac -> "osx"
+      else -> error("Unknown OS")
     }
 
-    private fun getJBRVersionFromSources(jbrFullVersion: String): JBRVersion {
-      val jbrVersion = jbrFullVersion.split(".")[0].toInt()
-      var majorVersion = jbrFullVersion.split("b").firstOrNull()
-      if (jbrVersion < 17) {
-        majorVersion = majorVersion?.replace(".", "_")
+    val arch = when (SystemInfo.isMac) {
+      true -> when (System.getProperty("os.arch")) {
+        "x86_64" -> "x64"
+        "aarch64" -> "aarch64"
+        else -> error("Unsupported architecture of Mac OS")
       }
-      requireNotNull(majorVersion) {
-        { "majorVersion is: $majorVersion" }
-      }
-      val buildNumber = jbrFullVersion.split("b").drop(1).singleOrNull()
-      requireNotNull(buildNumber) {
-        { "buildNumber is: $buildNumber" }
-      }
-      logOutput("Detected JBR version $jbrFullVersion with parts: $majorVersion and build $buildNumber")
-      return JBRVersion(majorVersion, buildNumber)
+      false -> "x64"
     }
 
-    fun downloadAndUnpackJbrFromBuildIfNeeded(jbrFullVersion: String): Path {
-      return downloadAndUnpackJbrIfNeeded(getJBRVersionFromBuild(jbrFullVersion))
+    val jbrFileName = "jbrsdk-$majorVersion-$os-$arch-b$buildNumber.tar.gz"
+    val path = try {
+      downloadJbr(jbrFileName)
+    }
+    catch (e: HttpClient.HttpNotFound) {
+      downloadJbr("jbrsdk_nomod-$majorVersion-$os-$arch-b$buildNumber.tar.gz")
     }
 
-    fun downloadAndUnpackJbrFromSourcesIfNeeded(jbrFullVersion: String): Path {
-      return downloadAndUnpackJbrIfNeeded(getJBRVersionFromSources(jbrFullVersion))
+    val appHome = (path.toFile().listFiles() ?: arrayOf()).singleOrNull { it.isDirectory }?.toPath()
+    requireNotNull(appHome) {
+      "appHome is null: $appHome"
     }
-
-    private fun downloadAndUnpackJbrIfNeeded(jbrVersion: JBRVersion): Path {
-      val (majorVersion, buildNumber) = listOf(jbrVersion.majorVersion, jbrVersion.buildNumber)
-
-      val os = when {
-        SystemInfo.isWindows -> "windows"
-        SystemInfo.isLinux -> "linux"
-        SystemInfo.isMac -> "osx"
-        else -> error("Unknown OS")
-      }
-
-      val arch = when (SystemInfo.isMac) {
-        true -> when (System.getProperty("os.arch")) {
-          "x86_64" -> "x64"
-          "aarch64" -> "aarch64"
-          else -> error("Unsupported architecture of Mac OS")
-        }
-        false -> "x64"
-      }
-
-      val jbrFileName = "jbrsdk-$majorVersion-$os-$arch-b$buildNumber.tar.gz"
-      val path = try {
-        downloadJbr(jbrFileName)
-      }
-      catch (e: HttpClient.HttpNotFound) {
-        downloadJbr("jbrsdk_nomod-$majorVersion-$os-$arch-b$buildNumber.tar.gz")
-      }
-
-      val appHome = (path.toFile().listFiles() ?: arrayOf()).singleOrNull { it.isDirectory }?.toPath()
-      requireNotNull(appHome) {
-        "appHome is null: $appHome"
-      }
-      when {
-        SystemInfo.isMac -> return appHome / "Contents" / "Home"
-      }
-      return appHome
+    when {
+      SystemInfo.isMac -> return appHome / "Contents" / "Home"
     }
+    return appHome
+  }
 
-    private fun downloadJbr(jbrFileName: String): Path {
-      val downloadUrl = "https://cache-redirector.jetbrains.com/intellij-jbr/$jbrFileName"
+  private fun downloadJbr(jbrFileName: String): Path {
+    val downloadUrl = "https://cache-redirector.jetbrains.com/intellij-jbr/$jbrFileName"
 
-      val jbrCacheDirectory = GlobalPaths.instance.getCacheDirectoryFor("jbr")
-      val localFile = jbrCacheDirectory / jbrFileName
-      val localDir = jbrCacheDirectory / jbrFileName.removeSuffix(".tar.gz")
+    val jbrCacheDirectory = GlobalPaths.instance.getCacheDirectoryFor("jbr")
+    val localFile = jbrCacheDirectory / jbrFileName
+    val localDir = jbrCacheDirectory / jbrFileName.removeSuffix(".tar.gz")
 
-      HttpClient.downloadIfMissing(downloadUrl, localFile, retries = 1, timeout = 5.minutes)
+    HttpClient.downloadIfMissing(downloadUrl, localFile, retries = 1, timeout = 5.minutes)
 
-      FileSystem.unpackIfMissing(localFile, localDir)
+    FileSystem.unpackIfMissing(localFile, localDir)
 
-      return localDir
-    }
+    return localDir
   }
 }
