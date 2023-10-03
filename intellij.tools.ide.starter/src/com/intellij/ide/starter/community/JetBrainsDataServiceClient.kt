6b34dd4f4a61ced66de06163f5e1ad7521869a47
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2023-10-03 13:55:55 +0300

    [ide starter] Move release filtering to ide starter. Fix for multiple same releases with different build number. Tests on release filtering.
    GitOrigin-RevId: 2539a455285da2eb268b9bea23fddde7d6c7164a

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/community/JetBrainsDataServiceClient.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/community/JetBrainsDataServiceClient.kt
@@ -10,25 +10,51 @@ import com.intellij.ide.starter.utils.logOutput
 import org.apache.http.client.methods.HttpGet
 
 
-class JetBrainsDataServiceClient {
-  companion object {
+object JetBrainsDataServiceClient {
+  private const val DATA_SERVICE_URL = "https://data.services.jetbrains.com"
 
-    private const val DATA_SERVICE_URL = "https://data.services.jetbrains.com"
+  fun getReleases(request: ProductInfoRequestParameters): Map<String, List<ReleaseInfo>> {
+    val getUrlToJbDataServices = "$DATA_SERVICE_URL/products/releases${request.toUriQuery()}"
+    logOutput("Requesting products by url: $getUrlToJbDataServices")
 
-    fun getReleases(request: ProductInfoRequestParameters): Map<String, List<ReleaseInfo>> {
-      val getUrlToJbDataServices = "$DATA_SERVICE_URL/products/releases${request.toUriQuery()}"
-      logOutput("Requesting products by url: $getUrlToJbDataServices")
-
-      return HttpClient.sendRequest(
-        HttpGet(getUrlToJbDataServices).apply {
-          addHeader("Content-Type", "application/json")
-          addHeader("Accept", "application/json")
-        }) {
-        jacksonObjectMapper()
-          .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-          .registerModule(JavaTimeModule())
-          .readValue(it.entity.content, object : TypeReference<Map<String, List<ReleaseInfo>>>() {})
-      }
+    return HttpClient.sendRequest(
+      HttpGet(getUrlToJbDataServices).apply {
+        addHeader("Content-Type", "application/json")
+        addHeader("Accept", "application/json")
+      }) {
+      jacksonObjectMapper()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .registerModule(JavaTimeModule())
+        .readValue(it.entity.content, object : TypeReference<Map<String, List<ReleaseInfo>>>() {})
     }
+  }
+
+  fun getLatestPublicReleases(productType: String,
+                              numberOfReleases: Int = Int.MAX_VALUE,
+                              maxReleaseBuild: String? = null): List<ReleaseInfo> {
+    return getReleases(ProductInfoRequestParameters(productType))
+      .values
+      .first()
+      .let {
+        if (maxReleaseBuild != null) it.filter { release -> release.build.substringBefore(".").toInt() <= maxReleaseBuild.toInt() } else it
+      }
+      .take(numberOfReleases)
+  }
+
+  /**
+   * @param productType What product releases do you need. UI/GO/AI etc.
+   * @param numberOfReleases How many latest releases do you need. 1/5/10 etc.
+   * @param maxReleaseBuild What is the newest release you need. 223/222/213 etc
+   *
+   * @return List of releases sorted from newest to oldest. 2023.1/2022.3 etc
+   * */
+  fun getLatestPublicReleaseVersions(productType: String,
+                                     numberOfReleases: Int = Int.MAX_VALUE,
+                                     maxReleaseBuild: String? = null): List<String> {
+    // because there might be multiple releases with the same major version we need to filter it as an additional step
+    return getLatestPublicReleases(productType, numberOfReleases = Int.MAX_VALUE, maxReleaseBuild)
+      .map { it.majorVersion }
+      .toSet()
+      .take(numberOfReleases)
   }
 }
