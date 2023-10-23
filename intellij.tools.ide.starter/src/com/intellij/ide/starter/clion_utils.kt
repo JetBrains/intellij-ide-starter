ff59b8f1168950972767351339186488d69eb33b
Mari Baburina <Maria.Baburina@jetbrains.com>
2023-10-23 20:01:44 +0200

    [cidr/radler] extracted test cases to the common place along with utilities
    GitOrigin-RevId: e52f894fa1aad826717d30337a864f0bd52c229d

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
@@ -1,6 +1,20 @@
 package com.intellij.ide.starter
 
 import com.intellij.ide.starter.ide.IDETestContext
+import com.intellij.ide.starter.ide.IdeProductProvider
+import com.intellij.ide.starter.models.IdeInfo
+import com.intellij.ide.starter.utils.Git
 
 fun IDETestContext.disableCMakeOpenProjectWizard() =
   applyVMOptionsPatch { this.addSystemProperty("clion.skip.open.wizard", "true") }
+
+// Should be passed manually (through TC or run configuration)
+val isRadler by lazy { System.getProperty("intellij.clion.radler.perf.tests", "false").toBoolean() }
+
+object CLionRadlerProductProvider {
+  val productInfo: IdeInfo
+    get() = if (isRadler) IdeProductProvider.Radler else IdeProductProvider.CL
+
+  private val IdeProductProvider.Radler
+    get() = CL.copy(buildType = "ijplatform_${Git.branch}_CIDR_CLion_InstallersWithRadler")
+}
