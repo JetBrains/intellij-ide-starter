a85dd70005911b7933b34024ffaa54864baa7115
Mari Baburina <Maria.Baburina@jetbrains.com>
2023-10-24 12:32:15 +0200

    [cidr/radler] identify CLion or Radler in CLionCases
    GitOrigin-RevId: d173a9eb0e49aade979fa832678272fb631b7b27

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
@@ -2,7 +2,6 @@ package com.intellij.ide.starter
 
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.models.IdeInfo
 import com.intellij.ide.starter.utils.Git
 
 fun IDETestContext.disableCMakeOpenProjectWizard() =
@@ -11,10 +10,5 @@ fun IDETestContext.disableCMakeOpenProjectWizard() =
 // Should be passed manually (through TC or run configuration)
 val isRadler by lazy { System.getProperty("intellij.clion.radler.perf.tests", "false").toBoolean() }
 
-object CLionRadlerProductProvider {
-  val productInfo: IdeInfo
-    get() = if (isRadler) IdeProductProvider.Radler else IdeProductProvider.CL
-
-  private val IdeProductProvider.Radler
-    get() = CL.copy(buildType = "ijplatform_${Git.branch}_CIDR_CLion_InstallersWithRadler")
-}
+val IdeProductProvider.Radler
+  get() = CL.copy(buildType = "ijplatform_${Git.branch}_CIDR_CLion_InstallersWithRadler")
