949220071f3eeffd49352fc438f80ac74051512f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-11-22 10:42:14 +0100

    GTW-6791 async telemetry shutdown
    GitOrigin-RevId: f36a8134d39797c0359374a94137a32f9f60dfeb

--- intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/collector/OpenTelemetrySpanCollector.kt
+++ intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/collector/OpenTelemetrySpanCollector.kt
@@ -3,15 +3,14 @@ package com.intellij.tools.ide.metrics.collector.starter.collector
 import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.runner.IDERunContext
 import com.intellij.tools.ide.metrics.collector.metrics.PerformanceMetrics
-import com.intellij.tools.ide.metrics.collector.telemetry.getMetricsFromSpanAndChildren
 import com.intellij.tools.ide.metrics.collector.telemetry.SpanFilter
+import com.intellij.tools.ide.metrics.collector.telemetry.getMetricsFromSpanAndChildren
 
 /**
  * Collect spans from opentelemetry.json and convert it to metrics, understandable by IJ Perf dashboard
  */
 class OpenTelemetrySpanCollector(val spanNames: List<String>) : MetricsCollector {
   override fun collect(runContext: IDERunContext): List<PerformanceMetrics.Metric> {
-    return getMetricsFromSpanAndChildren(runContext.logsDir.resolve(IDETestContext.OPENTELEMETRY_FILE).toFile(),
-                                         SpanFilter.containsNameIn(spanNames))
+    return getMetricsFromSpanAndChildren(runContext.logsDir.resolve(IDETestContext.OPENTELEMETRY_FILE), SpanFilter.containsNameIn(spanNames))
   }
 }
--- intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/collector/openTelemetry.kt
+++ intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/collector/openTelemetry.kt
@@ -3,10 +3,10 @@ package com.intellij.tools.ide.metrics.collector.starter.collector
 import com.intellij.ide.starter.ide.IDETestContext.Companion.OPENTELEMETRY_FILE
 import com.intellij.ide.starter.models.IDEStartResult
 import com.intellij.tools.ide.metrics.collector.metrics.PerformanceMetrics
-import com.intellij.tools.ide.metrics.collector.telemetry.getMetricsFromSpanAndChildren
 import com.intellij.tools.ide.metrics.collector.telemetry.SpanFilter
+import com.intellij.tools.ide.metrics.collector.telemetry.getMetricsFromSpanAndChildren
 
 fun getMetricsFromSpanAndChildren(startResult: IDEStartResult, filter: SpanFilter): List<PerformanceMetrics.Metric> {
-  val opentelemetryFile = startResult.runContext.logsDir.resolve(OPENTELEMETRY_FILE).toFile()
+  val opentelemetryFile = startResult.runContext.logsDir.resolve(OPENTELEMETRY_FILE)
   return getMetricsFromSpanAndChildren(opentelemetryFile, filter)
 }
