47321c3923880104b12e45514fedb784f8014680
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-01-11 15:02:12 +0100

    We can't copy app on mac file by file since it breaks the signature of the application and it can't be started
    GitOrigin-RevId: ac68e39a43f631c2512e7c28041ec6562627bc65

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/installer/ExistingIdeInstaller.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/ide/installer/ExistingIdeInstaller.kt
@@ -5,10 +5,13 @@ import com.intellij.ide.starter.ide.IdeInstaller
 import com.intellij.ide.starter.ide.InstalledIde
 import com.intellij.ide.starter.models.IdeInfo
 import com.intellij.ide.starter.path.GlobalPaths
+import com.intellij.ide.starter.process.exec.ProcessExecutor
+import com.intellij.openapi.util.SystemInfo
 import org.apache.commons.io.FileUtils
 import java.nio.file.Path
 import kotlin.io.path.div
 import kotlin.io.path.name
+import kotlin.time.Duration.Companion.minutes
 
 /**
  * Use existing installed IDE instead of downloading one.
@@ -28,7 +31,13 @@ class ExistingIdeInstaller(private val installedIdePath: Path) : IdeInstaller {
                        .getCacheDirectoryFor("builds") / "${ideInfo.productCode}-${ideInstaller.buildNumber}"
     installDir.toFile().deleteRecursively()
     val installedIde = installedIdePath.toFile()
-    FileUtils.copyDirectory(installedIde, installDir.resolve(installedIdePath.name).toFile())
+    val destDir = installDir.resolve(installedIdePath.name).toFile()
+    if (SystemInfo.isMac) {
+      ProcessExecutor("copy app", null, 5.minutes, emptyMap(), listOf("ditto", installedIde.absolutePath, destDir.absolutePath)).start()
+    }
+    else {
+      FileUtils.copyDirectory(installedIde, destDir)
+    }
     return Pair(
       ideInstaller.buildNumber,
       IdeDistributionFactory.installIDE(installDir.toFile(), ideInfo.executableFileName)
