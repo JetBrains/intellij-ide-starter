ad87fec5ce2d01e575d702ba9c8c8d96daa33379
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-02-26 10:55:27 +0100

    Rewrite GradleTest to Starter.newContext instead of DriverManager
    GitOrigin-RevId: 6492712c8279f59c7c431ba8a133163f2d42e7d5

---
+++ intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/contextCreationHelper.kt
@@ -0,0 +1,17 @@
+package com.intellij.ide.starter.junit5
+
+import com.intellij.ide.starter.ide.IDETestContext
+import com.intellij.ide.starter.models.IdeInfo
+import com.intellij.ide.starter.models.TestCase
+import com.intellij.ide.starter.project.NoProject
+import com.intellij.ide.starter.project.ProjectInfoSpec
+import com.intellij.ide.starter.runner.CurrentTestMethod
+import com.intellij.ide.starter.runner.Starter
+
+fun Starter.newContext(ideInfo: IdeInfo, project: ProjectInfoSpec = NoProject, systemProperties: Map<String, String>): IDETestContext =
+  newTestContainer().newContext(testName = CurrentTestMethod.hyphenateWithClass(), testCase = TestCase(ideInfo, project)).applyVMOptionsPatch {
+    systemProperties.forEach {
+      addSystemProperty(it.key, it.value)
+    }
+  }
+
