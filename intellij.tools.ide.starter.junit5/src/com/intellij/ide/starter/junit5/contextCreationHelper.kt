eec0303e923c5e9b88198df134604e2d442b6fdd
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-02-27 10:37:26 +0100

    Use builder instead of multiple arguments
    GitOrigin-RevId: 96004e5985ef25762417d5f68fd62d5f331b4c2e

--- intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/contextCreationHelper.kt
+++ intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/contextCreationHelper.kt
@@ -8,17 +8,16 @@ import com.intellij.ide.starter.project.ProjectInfoSpec
 import com.intellij.ide.starter.runner.CurrentDisplayName
 import com.intellij.ide.starter.runner.Starter
 
-fun Starter.newContext(ideInfo: IdeInfo, project: ProjectInfoSpec = NoProject, systemProperties: Map<String, String> = mapOf()): IDETestContext =
-  newTestContainer().newContext(testName = CurrentDisplayName.displayName(), testCase = TestCase(ideInfo, project)).applyVMOptionsPatch {
-    systemProperties.forEach {
+class IDETestContextConfig {
+  var project: ProjectInfoSpec = NoProject
+  var systemProperties: Map<String, String> = mapOf()
+}
+
+fun Starter.newContext(ideInfo: IdeInfo, configure: IDETestContextConfig.() -> Unit = {}): IDETestContext {
+  val config = IDETestContextConfig().apply(configure)
+  return newTestContainer().newContext(testName = CurrentDisplayName.displayName(), testCase = TestCase(ideInfo, config.project)).applyVMOptionsPatch {
+    config.systemProperties.forEach {
       addSystemProperty(it.key, it.value)
     }
   }
-
-fun Starter.newContext(testCase: TestCase<*>, systemProperties: Map<String, String> = mapOf()): IDETestContext =
-  newTestContainer().newContext(testName = CurrentDisplayName.displayName(), testCase = testCase).applyVMOptionsPatch {
-    systemProperties.forEach {
-      addSystemProperty(it.key, it.value)
-    }
-  }
-
+}
