af6ffdeaa32f4a446e08fabd66cce536b7acc6e3
Lev Zagnetin <lev.zagnetin@jetbrains.com>
2024-03-04 14:21:09 +0200

    [CLion Nova] perf tests: use single installer for performance tests
    GitOrigin-RevId: 9d142efe4e375723144c966b32f39f2c4b1b6757

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/clion_utils.kt
@@ -1,14 +1,9 @@
 package com.intellij.ide.starter
 
 import com.intellij.ide.starter.ide.IDETestContext
-import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.utils.Git
 
 fun IDETestContext.disableCMakeOpenProjectWizard() =
   applyVMOptionsPatch { this.addSystemProperty("clion.skip.open.wizard", "true") }
 
 // Should be passed manually (through TC or run configuration)
 val isRadler by lazy { System.getProperty("intellij.clion.radler.perf.tests", "false").toBoolean() }
-
-val IdeProductProvider.Radler
-  get() = CL.copy(buildType = "ijplatform_${Git.branch}_CIDR_CLion_InstallersWithRadler")
