a10bb3ca0b39db26f4948a54d3e7f8fadd5847f2
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2024-03-06 16:23:27 +0100

    Don't cache AllurePath from DI since this might lead to races
    GitOrigin-RevId: 07d4675ef59c6cd55283a2931b36d9b078ae1831

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/report/AllureWriter.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/report/AllureWriter.kt
@@ -5,11 +5,14 @@ import io.qameta.allure.AllureResultsWriter
 import io.qameta.allure.FileSystemResultsWriter
 import io.qameta.allure.model.TestResult
 import io.qameta.allure.model.TestResultContainer
+import org.kodein.di.direct
 import org.kodein.di.instance
 import java.io.InputStream
 
 object AllureWriter : AllureResultsWriter {
-  private val allurePath by di.instance<AllurePath>()
+  private val allurePath
+    get() = di.direct.instance<AllurePath>()
+
   override fun write(testResult: TestResult?) {
     FileSystemResultsWriter(allurePath.reportDir()).write(testResult)
   }
