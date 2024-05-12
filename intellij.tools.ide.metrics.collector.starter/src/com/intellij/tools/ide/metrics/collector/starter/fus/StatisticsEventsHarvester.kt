c330b888df7bf01d7505d9fac9db66251e9229fa
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2024-05-12 21:19:02 +0200

    AT-929 Allow FUS events json file not to exist with parameter (also AT-914)
    GitOrigin-RevId: 7b788e9020062ee94119a7185bda8671381d6b1d

--- intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/fus/StatisticsEventsHarvester.kt
+++ intellij.tools.ide.metrics.collector.starter/src/com/intellij/tools/ide/metrics/collector/starter/fus/StatisticsEventsHarvester.kt
@@ -18,20 +18,26 @@ class StatisticsEventsHarvester(private val eventLogsDir: Path) {
 
   constructor(testContext: IDETestContext, recorder: String = "FUS") : this(testContext.paths.systemDir.resolve("event-log-data/logs/$recorder"))
 
-  fun getStatisticEventsByGroup(groupId: String): List<LogEvent> = getStatisticEvents { it.group.id == groupId }
+  fun getStatisticEventsByGroup(groupId: String, failOnNoEventsWritten: Boolean = true): List<LogEvent> =
+    getStatisticEvents(failOnNoEventsWritten) { it.group.id == groupId }
 
   /**
    * Retrieve statistics events from all event log files at `system/event-log-data/logs/{recorder}`
    */
-  fun getStatisticEvents(filter: (LogEvent) -> Boolean = { true }): List<LogEvent> {
-    val logs = getStatisticsLogFiles()
+  fun getStatisticEvents(failOnNoEventsWritten: Boolean = true, filter: (LogEvent) -> Boolean = { true }): List<LogEvent> {
+    val logs = getStatisticsLogFiles(failOnNoEventsWritten)
     val allEvents = logs.flatMap { deserializeEventLogFile(it) }
 
     return allEvents.filter(filter)
   }
 
-  private fun getStatisticsLogFiles(): List<Path> {
-    require(eventLogsDir.exists()) { "Statistics logs folder must exists at $eventLogsDir" }
+  private fun getStatisticsLogFiles(failOnNoEventsWritten: Boolean): List<Path> {
+    if (failOnNoEventsWritten) {
+      require(eventLogsDir.exists()) { "Statistics logs folder must exists at $eventLogsDir" }
+    }
+    else if (!eventLogsDir.exists()) {
+      return emptyList()
+    }
 
     var logFiles = eventLogsDir.listDirectoryEntries(glob = "*.log")
     logFiles = logFiles.sortedWith { path1, path2 ->
