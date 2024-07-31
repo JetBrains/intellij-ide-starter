540fca62814feb8e537f8ad095090f72aa01d17c
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2024-07-31 14:18:18 +0300

    [starter] Do not print the wall of env variables on the local test run
    GitOrigin-RevId: 7d001c39c47e3d838c0485f7c3648c158d79a876

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/config/StarterConfigurationStorage.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/config/StarterConfigurationStorage.kt
@@ -1,5 +1,7 @@
 package com.intellij.ide.starter.config
 
+import com.intellij.ide.starter.ci.CIServer
+
 open class StarterConfigurationStorage : ConfigurationStorage() {
   companion object {
     const val ENV_ENABLE_CLASS_FILE_VERIFICATION = "ENABLE_CLASS_FILE_VERIFICATION"
@@ -40,6 +42,6 @@ open class StarterConfigurationStorage : ConfigurationStorage() {
     put(ENV_USE_LATEST_DOWNLOADED_IDE_BUILD, System.getenv(ENV_USE_LATEST_DOWNLOADED_IDE_BUILD))
     put(ENV_JUNIT_RUNNER_USE_INSTALLER, System.getenv(ENV_JUNIT_RUNNER_USE_INSTALLER))
     put(INSTALLER_INCLUDE_RUNTIME_MODULE_REPOSITORY, false)
-    put(ENV_LOG_ENVIRONMENT_VARIABLES, true)
+    put(ENV_LOG_ENVIRONMENT_VARIABLES, CIServer.instance.isBuildRunningOnCI)
   }
 }
