92b8ee13f485bbea4beba7e9b83b433136c268e2
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-07-31 11:07:13 +0200

    reduce usage of runWithSpan, remove useWithoutActiveScope, hide useWithoutActiveScope - coroutine-based API should be used instead
    GitOrigin-RevId: 455c5da850ce966f8103f9d330d3c6454e4f541e

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/telemetry/TestTelemetryService.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/telemetry/TestTelemetryService.kt
@@ -1,8 +1,8 @@
 package com.intellij.ide.starter.telemetry
 
 import com.intellij.ide.starter.di.di
-import io.opentelemetry.api.trace.SpanBuilder
 import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.SpanBuilder
 import io.opentelemetry.api.trace.StatusCode
 import io.opentelemetry.api.trace.Tracer
 import org.kodein.di.direct
@@ -12,9 +12,7 @@ interface TestTelemetryService {
   companion object {
     val instance: TestTelemetryService by lazy { di.direct.instance() }
 
-    fun spanBuilder(spanName: String): SpanBuilder {
-      return instance.getTracer().spanBuilder(spanName)
-    }
+    fun spanBuilder(spanName: String): SpanBuilder = instance.getTracer().spanBuilder(spanName)
   }
 
   fun getTracer(): Tracer
@@ -23,31 +21,18 @@ interface TestTelemetryService {
 }
 
 inline fun <T> computeWithSpan(spanName: String, operation: (Span) -> T): T {
-  return TestTelemetryService.spanBuilder(spanName).use(operation)
-}
-
-inline fun <T> SpanBuilder.use(operation: (Span) -> T): T {
-  return startSpan().useWithoutActiveScope { span ->
-    span.makeCurrent().use {
+  val span = TestTelemetryService.spanBuilder(spanName).startSpan()
+  try {
+    return span.makeCurrent().use {
       operation(span)
     }
   }
-}
-
-/**
- * Does not activate the span scope, so **new spans created inside will not be linked to [this] span**.
- * Consider using [use] to also activate the scope.
- */
-inline fun <T> Span.useWithoutActiveScope(operation: (Span) -> T): T {
-  try {
-    return operation(this)
-  }
   catch (e: Throwable) {
-    recordException(e)
-    setStatus(StatusCode.ERROR)
+    span.recordException(e)
+    span.setStatus(StatusCode.ERROR)
     throw e
   }
   finally {
-    end()
+    span.end()
   }
 }
