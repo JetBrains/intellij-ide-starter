8efc4f0401d81c14f4db3f67d4788efeca8b5da3
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2024-08-05 10:20:55 +0200

    Revert "[kotlin] use K1 mode in integration tests"
    This reverts commit fd2850fe78207bbbd2f8c8c5e8b4ba293927c16b.
    
    GitOrigin-RevId: 7a6c674fd8728fe08ba4cd67aa95eb1026d89971

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/utils/kotlinPluginMode.kt
+++
@@ -1,9 +0,0 @@
-package com.intellij.ide.starter.utils
-
-import com.intellij.ide.starter.ide.IDETestContext
-
-fun IDETestContext.setKotlinPluginMode(k2Mode: Boolean): IDETestContext {
-  return applyVMOptionsPatch {
-    addSystemProperty("idea.kotlin.plugin.use.k2", k2Mode.toString())
-  }
-}
