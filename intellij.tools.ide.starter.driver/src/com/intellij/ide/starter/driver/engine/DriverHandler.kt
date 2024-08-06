dba00f0e0f09f65be26b8b26a96e149cddcc6c8f
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-08-06 14:53:54 +0200

    driver properties: make host as InetAddress instead of String
    GitOrigin-RevId: 16a6f0f79576280310cc6a511cae6f88e33398e4

--- intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/engine/DriverHandler.kt
+++ intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/engine/DriverHandler.kt
@@ -15,15 +15,15 @@ open class DriverOptions {
 
 open class DriverHandler {
   companion object {
-    fun systemProperties(host: String = "127.0.0.1", port: Int): Map<String, String> {
+    fun systemProperties(host: InetAddress = InetAddress.getLoopbackAddress(), port: Int): Map<String, String> {
       // https://docs.oracle.com/javase/8/docs/technotes/guides/serialization/filters/serialization-filtering.html
       return mapOf(
         // the host name string that should be associated with remote stubs for locally created remote objects, in order to allow clients to invoke methods on the remote object,
         // if not specified: all interfaces the local host (127.0.0.1)
-        "java.rmi.server.hostname" to host,
+        "java.rmi.server.hostname" to host.hostAddress,
         // the bind address for the default JMX agent,
         // if not specified: all interfaces (0.0.0.0)
-        "com.sun.management.jmxremote.host" to host,
+        "com.sun.management.jmxremote.host" to host.hostAddress,
         "com.sun.management.jmxremote" to "true",
         "com.sun.management.jmxremote.port" to port.toString(),
         "com.sun.management.jmxremote.authenticate" to "false",
