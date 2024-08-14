311b46aa975edb2636f41842a8e8b9d83d5944ed
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-08-14 14:04:06 +0200

    use ScreenRecorder in RDCT tests
    GitOrigin-RevId: 7d90d3bb88e0218d2eeefdb13a8a751674700421

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/screenRecorder/ScreenRecorder.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/screenRecorder/ScreenRecorder.kt
@@ -7,38 +7,81 @@ import com.intellij.openapi.util.SystemInfo
 import com.intellij.tools.ide.util.common.logOutput
 import org.monte.media.Format
 import org.monte.media.FormatKeys.MediaType
+import org.monte.media.Registry
 import org.monte.media.VideoFormatKeys.*
 import org.monte.media.math.Rational
 import org.monte.screenrecorder.ScreenRecorder
 import java.awt.GraphicsEnvironment
 import java.awt.Rectangle
 import java.awt.Toolkit
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.io.path.createFile
 import kotlin.io.path.div
 import kotlin.io.path.pathString
 
 class IDEScreenRecorder(private val runContext: IDERunContext) {
+
+  companion object {
+    fun getScreenRecorder(movieFolder: File, filePrefix: String? = null) =
+      object : ScreenRecorder(
+        /* cfg = */
+        GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration,
+
+        /* captureArea = */
+        Rectangle(0, 0, Toolkit.getDefaultToolkit().screenSize.width, Toolkit.getDefaultToolkit().screenSize.height),
+
+        /* fileFormat = */
+        Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
+
+        /* screenFormat = */
+        Format(MediaTypeKey, MediaType.VIDEO,
+               EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+               CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+               DepthKey, 24,
+               FrameRateKey, Rational.valueOf(15.0),
+               QualityKey, 1.0f,
+               KeyFrameIntervalKey, 15 * 60),
+
+        /* mouseFormat = */
+        Format(MediaTypeKey, MediaType.VIDEO,
+               EncodingKey, "black",
+               FrameRateKey, Rational.valueOf(30.0)),
+
+
+        /* audioFormat = */
+        null,
+
+        /* movieFolder = */
+        movieFolder) {
+        override fun createMovieFile(fileFormat: Format): File {
+          if (filePrefix == null) {
+            return super.createMovieFile(fileFormat)
+          }
+          else {
+            return movieFolder.resolve(getMovieName(fileFormat, filePrefix))
+          }
+        }
+      }
+
+    private fun getMovieName(fileFormat: Format?, filePrefix: String?): String {
+      val dateFormat = SimpleDateFormat("yyyy-MM-dd 'at' HH.mm.ss")
+      val prefix = filePrefix ?: "ScreenRecording"
+      val extension = Registry.getInstance().getExtension(fileFormat)
+      val timestamp = dateFormat.format(Date())
+
+      return "$prefix ${timestamp}.$extension"
+    }
+  }
+
   var javaScreenRecorder: ScreenRecorder? = null
   var ffmpegProcess: Process? = null
 
   init {
     //on Linux, we run xvfb and test process is headless, so we need external tool to record screen
     if (!SystemInfo.isLinux) {
-      javaScreenRecorder = runCatching {
-        ScreenRecorder(
-          GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice.defaultConfiguration,
-          Rectangle(0, 0, Toolkit.getDefaultToolkit().screenSize.width,
-                    Toolkit.getDefaultToolkit().screenSize.height),
-          Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
-          Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
-                 CompressorNameKey,
-                 ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE, DepthKey, 24, FrameRateKey,
-                 Rational.valueOf(15.0), QualityKey, 1.0f,
-                 KeyFrameIntervalKey, 15 * 60),
-          Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, "black", FrameRateKey,
-                 Rational.valueOf(30.0)), null,
-          (runContext.logsDir / "screenRecording").toFile())
-      }.getOrNull()
+      javaScreenRecorder = runCatching { getScreenRecorder((runContext.logsDir / "screenRecording").toFile()) }.getOrNull()
     }
   }
 
