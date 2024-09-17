54721997c30908cfed79cf5c57eca6de1622ee39
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2024-09-17 14:34:56 +0200

    [driver-split-tests] add info logs to the output
    GitOrigin-RevId: d69ef6069f01b6d955d5c9f67b03dc51a0774492

--- intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/driver/remoteDev/RemDevDriverRunner.kt
+++ intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/driver/remoteDev/RemDevDriverRunner.kt
@@ -10,10 +10,14 @@ import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.runner.IDECommandLine
 import com.intellij.ide.starter.runner.IDERunContext
 import com.intellij.ide.starter.runner.events.IdeBeforeLaunchEvent
+import com.intellij.openapi.diagnostic.IdeaLogRecordFormatter
 import com.intellij.openapi.diagnostic.LogLevel
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.tools.ide.performanceTesting.commands.MarshallableCommand
 import com.intellij.tools.ide.starter.bus.EventsBus
 import com.intellij.tools.ide.util.common.logOutput
+import java.util.logging.ConsoleHandler
+import java.util.logging.Level
 import kotlin.time.Duration
 
 const val REQUIRE_FLUXBOX_VARIABLE = "REQUIRE_FLUXBOX"
@@ -21,6 +25,8 @@ const val REQUIRE_FLUXBOX_VARIABLE = "REQUIRE_FLUXBOX"
 class RemDevDriverRunner : DriverRunner {
   override fun runIdeWithDriver(context: IDETestContext, commandLine: (IDERunContext) -> IDECommandLine, commands: Iterable<MarshallableCommand>, runTimeout: Duration, useStartupScript: Boolean, launchName: String, expectedKill: Boolean, expectedExitCode: Int, collectNativeThreads: Boolean, configure: IDERunContext.() -> Unit): BackgroundRun {
     require(context is IDERemDevTestContext) { "for split-mode context should be instance of ${IDERemDevTestContext::class.java.simpleName}" }
+
+    addConsoleAllAppender()
 
     val remoteDevDriverOptions = RemoteDevDriverOptions()
     addRemoteDevSpecificTraces(context)
@@ -44,5 +50,23 @@ class RemDevDriverRunner : DriverRunner {
     context.applyVMOptionsPatch {
       configureLoggers(LogLevel.TRACE, "jb.focus.requests")
     }
+  }
+
+  companion object {
+    private val consoleAppender = ConsoleHandler().apply {
+      formatter = IdeaLogRecordFormatter()
+    }
+  }
+
+  private fun addConsoleAllAppender() {
+    Logger.getInstance("") // force to initialize logger model
+    val root = java.util.logging.Logger.getLogger("")
+    val oldConsoleHandler = root.handlers.find { it is ConsoleHandler }
+    if (oldConsoleHandler != null) {
+      root.removeHandler(oldConsoleHandler)
+    }
+    // change to All for local debug
+    root.level = Level.INFO
+    root.addHandler(consoleAppender)
   }
 }
