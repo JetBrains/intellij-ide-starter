e792806132e6e377119a4e5d8c83b2b08a17c513
Anna Koehler <anna.marchenko@jetbrains.com>
2024-09-19 11:00:54 +0200

    CLion integration tests: remove usage of system property use.branch.name since CLion downloaded now from the default branch and we do not need previous workarounds in the code where we download an Installer during local run of any integration test (now it leads to the situation where we download a last successful build from any branch of a given configuration)
    GitOrigin-RevId: e897022eff79b5570558bd2718eeee9fceb0c176

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/ci/teamcity/TeamCityClient.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/ci/teamcity/TeamCityClient.kt
@@ -68,9 +68,7 @@ object TeamCityClient {
   fun getLastSuccessfulBuild(ideInfo: IdeInfo): Pair<String, String> {
     val tag = if (!ideInfo.tag.isNullOrBlank()) "tag:${ideInfo.tag}," else ""
     val number = if (!ideInfo.buildNumber.isBlank()) "number:${ideInfo.buildNumber}," else ""
-    val branchName = System.getProperty("use.branch.name", "")
-    val branch = if (branchName.isNotEmpty()) "branch:$branchName," else "branch:default:any,"
-    val fullUrl = guestAuthUri.resolve("builds?locator=buildType:${ideInfo.buildType},${branch}${tag}${number}status:SUCCESS,state:(finished:true),count:1")
+    val fullUrl = guestAuthUri.resolve("builds?locator=buildType:${ideInfo.buildType},${tag}${number}status:SUCCESS,state:(finished:true),count:1")
 
     val build = get(fullUrl).fields().asSequence().first { it.key == "build" }.value
     val buildId = build.findValue("id").asText()
