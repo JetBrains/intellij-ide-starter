4b8a2109468a19741a605255cf33642cae2932ac
Nikita Barkov <nikita.barkov@jetbrains.com>
2024-09-30 20:03:49 +0200

    [fuc_tests]AT-1219. Updated InstallPluginAfterUpdateIdeTest
    GitOrigin-RevId: d41881a40189bf12d68cb9f30d24d87dac4cb936

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/tests/InstallPluginAfterUpdateIdeTest.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/tests/InstallPluginAfterUpdateIdeTest.kt
@@ -5,10 +5,10 @@ import com.intellij.ide.starter.ci.CIServer
 import com.intellij.ide.starter.ci.teamcity.TeamCityClient
 import com.intellij.ide.starter.ci.teamcity.asTeamCity
 import com.intellij.ide.starter.ci.teamcity.withAuth
+import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.ide.IdeProductProvider.IU
 import com.intellij.ide.starter.junit5.config.KillOutdatedProcesses
 import com.intellij.ide.starter.models.TestCase
-import com.intellij.ide.starter.project.RemoteArchiveProjectInfo
 import com.intellij.ide.starter.report.ErrorReporterToCI
 import com.intellij.ide.starter.runner.Starter
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
@@ -17,6 +17,7 @@ import com.intellij.tools.plugin.checker.data.TestCases
 import com.intellij.tools.plugin.checker.di.initPluginCheckerDI
 import com.intellij.tools.plugin.checker.marketplace.MarketplaceClient
 import com.intellij.tools.plugin.checker.marketplace.Plugin
+import com.intellij.util.containers.ContainerUtil.subtract
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -26,7 +27,12 @@ import kotlin.io.path.createFile
 
 @ExtendWith(KillOutdatedProcesses::class)
 class InstallPluginAfterUpdateIdeTest {
-  private data class ConfigurationData(val type: String, val version: String, val url: String, val batchesCount: Int, val currentBatchIndex: Int)
+  private data class ConfigurationData(
+    val type: String,
+    val url: String,
+    val currentBatchIndex: Int,
+  )
+
   companion object {
     private fun JsonNode.getProperty(name: String): String {
       return this
@@ -43,58 +49,80 @@ class InstallPluginAfterUpdateIdeTest {
       }
 
       val propertyNode = buildProperties.path("property")
-      initPluginCheckerDI()
 
       return ConfigurationData(
         propertyNode.getProperty("ide.type"),
-        propertyNode.getProperty("ide.version"),
         propertyNode.getProperty("ide.download.url"),
-        propertyNode.getProperty("teamcity.build.parallelTests.totalBatches").toInt(),
-        propertyNode.getProperty("teamcity.build.parallelTests.currentBatch").toInt() - 1,
+        propertyNode.getProperty("batch.index").toInt(),
       )
     }
 
 
-    private fun <T> splitIntoBuckets(list: List<T>, bucketCount: Int): List<List<T>> {
-      val bucketSize = list.size / bucketCount
-      val remainder = list.size % bucketCount
-      return (0 until bucketCount).map { i ->
+    private fun <T> splitIntoBuckets(list: List<T>): List<List<T>> {
+      val batchesCount = 10
+      val bucketSize = list.size / batchesCount
+      val remainder = list.size % batchesCount
+      return (0 until batchesCount).map { i ->
         val start = i * bucketSize + minOf(i, remainder)
         val end = (i + 1) * bucketSize + minOf(i + 1, remainder)
         list.subList(start, end)
       }
     }
 
+    private fun createTestContext(case: TestCase<*>, configurator: IDETestContext.() -> Unit = {}): IDETestContext {
+      val testContext = Starter
+        .newContext(testName = "install plugin test", testCase = case)
+        .prepareProjectCleanImport()
+        .setSharedIndexesDownload(enable = true)
+        .setLicense(System.getenv("LICENSE_KEY"))
+
+      testContext.configurator()
+
+      return testContext
+    }
+
     @JvmStatic
     fun pluginsProvider(): List<Arguments> {
+      initPluginCheckerDI()
       val configurationData = getConfigurationData()
       val case = TestCases.IU.GradleJitPackSimple
         .copy(
           ideInfo = IU.copy(
-            buildNumber = configurationData.version,
             downloadURI = URI(configurationData.url)
           )
         )
-      val plugins = MarketplaceClient.getPluginsForBuild(configurationData.type, configurationData.version).take(20)
+
+      val context = createTestContext(case)
+
+      val plugins = MarketplaceClient.getPluginsForBuild(configurationData.type, context.ide.build)
 
 
-      val pluginsForThisBucket = splitIntoBuckets(plugins, configurationData.batchesCount)[configurationData.currentBatchIndex]
+      println("Current batch index: ${configurationData.currentBatchIndex}")
+      val pluginsForThisBucket = splitIntoBuckets(plugins)[configurationData.currentBatchIndex]
 
-      return pluginsForThisBucket.map { Arguments.of(case to it, it.name) }
+      return pluginsForThisBucket.map { Arguments.of(context to it, it.name) }
     }
   }
 
 
-  @ParameterizedTest(name = "Validate plugin {1}")
+  @ParameterizedTest(name = "{1}")
   @MethodSource("pluginsProvider")
-  fun validatePlugin(data: Pair<TestCase<RemoteArchiveProjectInfo>, Plugin>, pluginName: String) {
-    val (case, plugin) = data
-    val context = Starter.newContext(testName = "install_plugin_${data.second.id}", testCase = case)
-    val pluginPath = context.paths.testHome.resolve(plugin.id + ".zip").createFile()
+  fun validatePlugin(data: Pair<IDETestContext, Plugin>, pluginName: String) {
+    val (context, plugin) = data
+    val contextWithPlugin = createTestContext(context.testCase)
+    val pluginPath = contextWithPlugin.paths.testHome.resolve(plugin.id + ".zip").createFile()
     MarketplaceClient.downloadPlugin(plugin, pluginPath.toFile())
-    context.apply { pluginConfigurator.installPluginFromPath(pluginPath) }
-    val runContext = context.runIDE(commands = CommandChain().exitApp()).runContext
-    val errors = ErrorReporterToCI.collectErrors(runContext.logsDir)
-    ErrorReporterToCI.reportErrors(runContext, errors)
+    contextWithPlugin.apply { pluginConfigurator.installPluginFromPath(pluginPath) }
+
+    val ideRunContextWithoutPlugin =
+      context.runIDE(launchName = "Run without plugin", commands = CommandChain().exitApp()).runContext
+    val errorsWithoutPlugin = ErrorReporterToCI.collectErrors(ideRunContextWithoutPlugin.logsDir)
+
+    val ideRunContext =
+      contextWithPlugin.runIDE(launchName = "Run with plugin", commands = CommandChain().exitApp()).runContext
+    val errorsWithPlugin = ErrorReporterToCI.collectErrors(ideRunContext.logsDir)
+
+    val diff = subtract(errorsWithPlugin, errorsWithoutPlugin).toList()
+    ErrorReporterToCI.reportErrors(ideRunContext, diff)
   }
 }
