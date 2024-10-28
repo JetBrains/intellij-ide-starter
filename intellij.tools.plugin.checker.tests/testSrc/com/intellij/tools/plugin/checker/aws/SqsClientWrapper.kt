8b675eacab71c7d9641b12e9144dec164cd74f28
Nikita Barkov <nikita.barkov@jetbrains.com>
2024-10-28 09:51:34 +0100

    [functional_tests]Added validation into client
    GitOrigin-RevId: c20ffb11e168b3c98ab4d0f3a92072e4cf109ebd

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/aws/SqsClientWrapper.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/aws/SqsClientWrapper.kt
@@ -8,23 +8,38 @@ import software.amazon.awssdk.services.sqs.SqsClient
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest
 
 class SqsClientWrapper(private val queueUrl: String,  region: Region) {
-    private val awsAccessKey: String = System.getenv("AWS_ACCESS_KEY_ID")
-    private val awsSecretKey: String = System.getenv("AWS_SECRET_ACCESS_KEY")
+  private val awsAccessKey: String = System.getenv("AWS_ACCESS_KEY_ID")
+                                     ?: throw IllegalArgumentException("AWS_ACCESS_KEY_ID is not set in the environment variables")
 
-    private val sqsClient: SqsClient = SqsClient.builder()
-        .region(region)
-        .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsSecretKey)))
+  private val awsSecretKey: String = System.getenv("AWS_SECRET_ACCESS_KEY")
+                                     ?: throw IllegalArgumentException("AWS_SECRET_ACCESS_KEY is not set in the environment variables")
+
+  private val sqsClient: SqsClient by lazy {
+    SqsClient.builder()
+      .region(region)
+      .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(awsAccessKey, awsSecretKey)))
+      .build()
+  }
+
+  private val mapper = jacksonObjectMapper()
+
+  fun sendMessage(message: VerificationMessage) {
+    try {
+      val messageBody = mapper.writeValueAsString(message)
+      val sendMsgRequest = SendMessageRequest.builder()
+        .queueUrl(queueUrl)
+        .messageBody(messageBody)
         .build()
 
-    fun sendMessage(message: VerificationMessage) {
-        val mapper = jacksonObjectMapper()
-        val messageBody = mapper.writeValueAsString(message)
-
-        val sendMsgRequest = SendMessageRequest.builder()
-            .queueUrl(queueUrl)
-            .messageBody(messageBody)
-            .build()
-
-        sqsClient.sendMessage(sendMsgRequest)
+      sqsClient.sendMessage(sendMsgRequest)
+      println("Message sent successfully.")
+    } catch (e: Exception) {
+      println("Failed to send message: ${e.message}")
+      e.printStackTrace()
     }
+  }
+
+  fun closeClient() {
+    sqsClient.close()
+  }
 }
