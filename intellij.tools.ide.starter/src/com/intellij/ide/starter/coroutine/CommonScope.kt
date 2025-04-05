53cd291bd5bcfbe199b133e58929fb917aca1885
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-04-05 18:22:50 +0200

    [starter] introduce perClassSupervisorScope
     IJ-CR-159619
    
    GitOrigin-RevId: 6f4a235efca583af78579b875b6f9de62a8ed8b1

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/coroutine/CommonScope.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/coroutine/CommonScope.kt
@@ -1,5 +1,6 @@
 package com.intellij.ide.starter.coroutine
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -10,28 +11,28 @@ import kotlinx.coroutines.SupervisorJob
  * Unhandled exceptions in the tree of child coroutines will not affect any other coroutines (parent included).
  * Usually that what you need.
  */
-val testSuiteSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+val testSuiteSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName("Test suite's supervisor scope"))
 
 /**
  * Lifespan is limited to duration each test. By the end of the test whole coroutines tree will be cancelled.
  * Unhandled exceptions in the tree of child coroutines will not affect any other coroutines (parent included).
  * Usually that what you need.
  */
-val perTestSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+val perTestSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName("Test's supervisor scope"))
 
 /**
  * Lifespan is limited to duration of the test class. By the end of the test class whole coroutines tree will be cancelled.
  */
-val perClassSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+val perClassSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName("Test class's supervisor scope"))
 
 /**
  * Lifespan is limited to the duration of a single thin client execution. When the client is shut down, the whole coroutines tree is going
  * to be canceled.
  */
-val perClientSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+val perClientSupervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO + CoroutineName("Client's supervisor scope"))
 
 /**
  * In case of unhandled exception in child coroutines all the coroutines tree (parents and other branches) will be cancelled.
  * In most scenarious you don't need that behaviour.
  */
-val simpleScope = CoroutineScope(Job() + Dispatchers.IO)
+val simpleScope = CoroutineScope(Job() + Dispatchers.IO + CoroutineName("Simple scope"))
