cfb578ddcd4bf56a5e249de2961529f480463340
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-04-15 14:39:15 +0200

    [rdct-tests] AT-2253 workaround to prevent junit5 from running tests during shutdown
    GitOrigin-RevId: b71680c8f2936ca1a2f42c423768207491fcb636

---
+++ intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/ShutdownListener.kt
@@ -0,0 +1,28 @@
+package com.intellij.ide.starter.junit5
+
+import com.intellij.tools.ide.util.common.logOutput
+import org.junit.jupiter.api.extension.ConditionEvaluationResult
+import org.junit.jupiter.api.extension.ExecutionCondition
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class ShutdownListener : ExecutionCondition {
+
+  companion object {
+    var shuttingDown: Boolean = false
+  }
+
+  init {
+    Runtime.getRuntime().addShutdownHook(Thread(Runnable { shuttingDown = true }, "Shutdown-indicator"))
+  }
+
+  override fun evaluateExecutionCondition(context: ExtensionContext?): ConditionEvaluationResult? {
+    return if (shuttingDown) {
+      val message = "Execution of '${context?.displayName}' cannot begin because shutdown is in progress, see AT-2253"
+      logOutput(message)
+      ConditionEvaluationResult.disabled(message)
+    }
+    else {
+      ConditionEvaluationResult.enabled("Test execution can begin")
+    }
+  }
+}
