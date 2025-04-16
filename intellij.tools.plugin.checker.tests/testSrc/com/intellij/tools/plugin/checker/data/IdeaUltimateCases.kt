b1474a2f520d17df474c60cc83ca5d02b87746d5
Eugene Morozov <eugene.morozov@jetbrains.com>
2025-04-16 19:23:46 +0200

    Revert "[plugin checker tests] Check out projects via Git to prevent the IDE from looking up a parent VCS root"
    This reverts commit da207fceef0b604dd27bbdf59b64f83c3015a53d.
    
    GitOrigin-RevId: 7f6c8f5a565f65163ec67dc80fc76ee2cd5d9a27

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/IdeaUltimateCases.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/IdeaUltimateCases.kt
@@ -1,14 +1,15 @@
 package com.intellij.tools.plugin.checker.data
 
 import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.project.GitHubProject
 import com.intellij.ide.starter.project.RemoteArchiveProjectInfo
 import com.intellij.ide.starter.project.TestCaseTemplate
 
 object IdeaUltimateCases : TestCaseTemplate(IdeProductProvider.IU) {
 
   val GradleJitPackSimple = withProject(
-    GitHubProject.fromGithub(repoRelativeUrl = "jitpack/gradle-simple")
+    RemoteArchiveProjectInfo(
+      projectURL = "https://github.com/jitpack/gradle-simple/archive/refs/heads/master.zip",
+    )
   )
 
   val MavenSimpleApp = withProject(
--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/PhpStormCases.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/PhpStormCases.kt
@@ -1,11 +1,13 @@
 package com.intellij.tools.plugin.checker.data
 
 import com.intellij.ide.starter.ide.IdeProductProvider
-import com.intellij.ide.starter.project.GitHubProject
+import com.intellij.ide.starter.project.RemoteArchiveProjectInfo
 import com.intellij.ide.starter.project.TestCaseTemplate
 
 object PhpStormCases : TestCaseTemplate(IdeProductProvider.PS) {
   val LaravelFramework = withProject(
-    GitHubProject.fromGithub(repoRelativeUrl = "laravel/framework")
+    RemoteArchiveProjectInfo(
+      projectURL = "https://github.com/laravel/framework/archive/refs/heads/master.zip",
+    )
   )
 }
