2968a999532813b5846b30e57c7aa3a9728fec9c
Eugene Morozov <eugene.morozov@jetbrains.com>
2025-06-27 13:45:15 +0200

    AT-2904 Optimize the test to reuse the IDE run without a plugin instead of running it for each plugin separately
    GitOrigin-RevId: ee2ea5136180c9e9704eb1c35bf6bb287ca9151b

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/tests/InstallPluginAfterUpdateIdeTest.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/tests/InstallPluginAfterUpdateIdeTest.kt
@@ -9,6 +9,7 @@ import com.intellij.ide.starter.ide.IDETestContext
 import com.intellij.ide.starter.ide.IdeProductProvider.IU
 import com.intellij.ide.starter.junit5.config.KillOutdatedProcesses
 import com.intellij.ide.starter.models.TestCase
+import com.intellij.ide.starter.report.Error
 import com.intellij.ide.starter.report.ErrorReporterToCI
 import com.intellij.ide.starter.runner.Starter
 import com.intellij.tools.ide.performanceTesting.commands.CommandChain
@@ -18,6 +19,7 @@ import com.intellij.tools.plugin.checker.di.initPluginCheckerDI
 import com.intellij.tools.plugin.checker.marketplace.MarketplaceClient
 import com.intellij.tools.plugin.checker.marketplace.Plugin
 import com.intellij.util.containers.ContainerUtil.subtract
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -34,6 +36,22 @@ class InstallPluginAfterUpdateIdeTest {
   )
 
   companion object {
+    private val configurationData: ConfigurationData = getConfigurationData()
+    private val testContextWithoutPlugin: IDETestContext = createTestContext(
+      TestCases.IU.GradleJitPackSimple.copy(
+        ideInfo = IU.copy(downloadURI = URI(configurationData.url))
+      )
+    )
+    private var errorsWithoutPlugin: List<Error>? = null
+
+    @BeforeAll
+    @JvmStatic
+    fun setupBaseline() {
+      initPluginCheckerDI()
+      val runResult = testContextWithoutPlugin.runIDE(launchName = "without-plugin", commands = CommandChain().exitApp())
+      errorsWithoutPlugin = ErrorReporterToCI.collectErrors(runResult.runContext.logsDir)
+    }
+    
     private fun JsonNode.getProperty(name: String): String {
       return this
         .first { it.get("name").asText() == name }
@@ -71,7 +89,7 @@ class InstallPluginAfterUpdateIdeTest {
 
     private fun createTestContext(case: TestCase<*>, configurator: IDETestContext.() -> Unit = {}): IDETestContext {
       val testContext = Starter
-        .newContext(testName = "install plugin test", testCase = case)
+        .newContext(testName = "new-installer-plugins-test", testCase = case)
         .prepareProjectCleanImport()
         .setSharedIndexesDownload(enable = true)
         .setLicense(System.getenv("LICENSE_KEY"))
@@ -83,24 +101,11 @@ class InstallPluginAfterUpdateIdeTest {
 
     @JvmStatic
     fun pluginsProvider(): List<Arguments> {
-      initPluginCheckerDI()
-      val configurationData = getConfigurationData()
-      val case = TestCases.IU.GradleJitPackSimple
-        .copy(
-          ideInfo = IU.copy(
-            downloadURI = URI(configurationData.url)
-          )
-        )
-
-      val context = createTestContext(case)
-
-      val plugins = MarketplaceClient.getPluginsForBuild(configurationData.type, context.ide.build)
-
-
+      val plugins = MarketplaceClient.getPluginsForBuild(configurationData.type, testContextWithoutPlugin.ide.build)
       println("Current batch index: ${configurationData.currentBatchIndex}")
       val pluginsForThisBucket = splitIntoBuckets(plugins)[configurationData.currentBatchIndex]
 
-      return pluginsForThisBucket.map { Arguments.of(context to it, it.name) }
+      return pluginsForThisBucket.map { Arguments.of(testContextWithoutPlugin to it, it.name) }
     }
   }
 
@@ -114,15 +119,11 @@ class InstallPluginAfterUpdateIdeTest {
     MarketplaceClient.downloadPlugin(plugin, pluginPath.toFile())
     contextWithPlugin.apply { pluginConfigurator.installPluginFromPath(pluginPath) }
 
-    val ideRunContextWithoutPlugin =
-      context.runIDE(launchName = "Run without plugin", commands = CommandChain().exitApp()).runContext
-    val errorsWithoutPlugin = ErrorReporterToCI.collectErrors(ideRunContextWithoutPlugin.logsDir)
-
     val ideRunContext =
-      contextWithPlugin.runIDE(launchName = "Run with plugin", commands = CommandChain().exitApp()).runContext
+      contextWithPlugin.runIDE(launchName = "with-plugin-${plugin.id}", commands = CommandChain().exitApp()).runContext
     val errorsWithPlugin = ErrorReporterToCI.collectErrors(ideRunContext.logsDir)
 
-    val diff = subtract(errorsWithPlugin, errorsWithoutPlugin).toList()
+    val diff = subtract(errorsWithPlugin, errorsWithoutPlugin!!).toList()
     ErrorReporterToCI.reportErrors(ideRunContext, diff)
   }
 }
