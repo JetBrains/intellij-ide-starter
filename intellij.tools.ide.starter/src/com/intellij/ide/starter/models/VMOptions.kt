6fa0fe7bed0e3eb04a257990461f93b4b8f07566
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2025-07-24 12:55:47 +0200

    AT-2984 Make integration tests start full-function single-distro IDE by default
    GitOrigin-RevId: a74a5145ad42bb9a1a74b157597805bd409babae

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/models/VMOptions.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/models/VMOptions.kt
@@ -224,6 +224,10 @@ data class VMOptions(
     addSystemProperty("writerside.eula.reviewed.and.accepted", true)
   }
 
+  fun dontDisablePaidPluginsOnStartup() {
+    addSystemProperty("ide.do.not.disable.paid.plugins.on.startup", true)
+  }
+
   fun disableNewUsersOnboardingDialogue() {
     addSystemProperty("ide.newUsersOnboarding", false)
   }
