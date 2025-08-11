be57d811bdee74d77566d863087b23ad79b5fb4a
Eugene Morozov <eugene.morozov@jetbrains.com>
2025-08-11 18:21:49 +0200

    AT-2904 Switch MarketplaceClient to the newer endpoint; exclude themes from the test
    GitOrigin-RevId: 6604d956a3677c283c4f78878347d580f7b12828

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/marketplace/MarketplaceClient.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/marketplace/MarketplaceClient.kt
@@ -1,48 +1,53 @@
 package com.intellij.tools.plugin.checker.marketplace
 
-
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.HttpEntity
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import org.w3c.dom.Element
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
-import java.net.URLEncoder
-import javax.xml.parsers.DocumentBuilderFactory
 
 data class Plugin(
     val id: String,
     val name: String,
-    val version: String
+    val updateId: String
 )
 
 object MarketplaceClient {
     private const val BASE_URL = "https://plugins.jetbrains.com"
     private val client: CloseableHttpClient = HttpClients.createDefault()
+    private val objectMapper = ObjectMapper()
 
     fun getPluginsForBuild(productCode: String, build: String): List<Plugin> {
-        val url = "$BASE_URL/plugins/list/?build=${productCode}-${build}"
-        val httpGet = HttpGet(url)
+        val pluginsUrl = "$BASE_URL/api/search/plugins?build=${productCode}-${build}&pricingModels=FREE&max=10000"
 
+        val pluginsResponse = client.execute(HttpGet(pluginsUrl))
+        val pluginsEntity: HttpEntity = pluginsResponse.entity ?: throw IOException("Empty response body")
 
-        val response = client.execute(httpGet)
-        val entity: HttpEntity = response.entity ?: throw IOException("Empty response body")
-
-        return if (response.statusLine.statusCode == 200) {
-            val xmlResponse = EntityUtils.toString(entity)
-            parsePluginsFromXml(xmlResponse)
-        } else {
-            throw IOException("Failed to get plugins: ${response.statusLine.statusCode}")
+        if (pluginsResponse.statusLine.statusCode != 200) {
+            throw IOException("Failed to get plugins: ${pluginsResponse.statusLine.statusCode}")
         }
+        val allPlugins = parsePluginsFromJson(EntityUtils.toString(pluginsEntity))
+
+        val themesUrl = "$pluginsUrl&tags=THEME"
+        val themesResponse = client.execute(HttpGet(themesUrl))
+        val themesEntity: HttpEntity = themesResponse.entity ?: throw IOException("Empty response body")
+        if (themesResponse.statusLine.statusCode != 200) {
+            throw IOException("Failed to get themes: ${themesResponse.statusLine.statusCode}")
+        }
+        val themePlugins = parsePluginsFromJson(EntityUtils.toString(themesEntity))
+        val themeIds = themePlugins.map { it.id }.toHashSet()
+
+        return allPlugins.filter { it.id !in themeIds }
     }
 
     fun downloadPlugin(plugin: Plugin, destinationZip: File) {
-        val url = "$BASE_URL/plugin/download?pluginId=${URLEncoder.encode(plugin.id)}&version=${plugin.version}"
+        val url = "$BASE_URL/plugin/download?updateId=${plugin.updateId}&noStatistic=true"
         val httpGet = HttpGet(url)
-
 
         val response = client.execute(httpGet)
         if (response.statusLine.statusCode != 200) {
@@ -57,31 +62,23 @@ object MarketplaceClient {
         }
     }
 
-    private fun parsePluginsFromXml(xml: String): List<Plugin> {
-        val factory = DocumentBuilderFactory.newInstance()
-        val builder = factory.newDocumentBuilder()
-        val xmlInput = xml.byteInputStream()
-        val document = builder.parse(xmlInput)
-
+    private fun parsePluginsFromJson(json: String): List<Plugin> {
+        val root: JsonNode = objectMapper.readTree(json)
         val pluginList = mutableListOf<Plugin>()
-        val pluginNodes = document.getElementsByTagName("idea-plugin")
-
-        for (i in 0 until pluginNodes.length) {
-            val pluginElement = pluginNodes.item(i) as Element
-
-            val name = pluginElement.getElementsByTagName("name").item(0).textContent
-            val id = pluginElement.getElementsByTagName("id").item(0).textContent
-            val version = pluginElement.getElementsByTagName("version").item(0).textContent
-
-            val plugin = Plugin(
-                id = id,
-                name = name,
-                version = version
-            )
-
-            pluginList.add(plugin)
+        if (root.isArray) {
+            for (node in root) {
+                val id = node.path("id")!!.asText()
+                val xmlId = node.path("xmlId")!!.asText()
+                val updateId = node.path("updateId")!!.asText()
+              pluginList.add(
+                Plugin(
+                  id = id,
+                  name = xmlId,
+                  updateId = updateId
+                )
+              )
+            }
         }
-
         return pluginList
     }
 }
