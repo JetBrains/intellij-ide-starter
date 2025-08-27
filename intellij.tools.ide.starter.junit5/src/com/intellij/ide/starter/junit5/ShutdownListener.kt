1aa370db50b86f98e679d2c7b5db37408366521d
roman.ivanitskii <roman.ivanitskii@jetbrains.com>
2025-08-27 17:56:25 +0200

    [starter][integration-tests] Add Allure reporting for shutdown tests
    Add failure reporting to Allure when test execution is blocked due to shutdown in progress, addressing debug needs in AT-2253.
    
    GitOrigin-RevId: 31ecb0e655161ae8c1009e9b216fd45d2cb290fe

--- intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/ShutdownListener.kt
+++ intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/ShutdownListener.kt
@@ -1,5 +1,6 @@
 package com.intellij.ide.starter.junit5
 
+import com.intellij.ide.starter.report.AllureReport
 import com.intellij.tools.ide.util.common.logOutput
 import org.junit.jupiter.api.extension.ConditionEvaluationResult
 import org.junit.jupiter.api.extension.ExecutionCondition
@@ -22,6 +23,10 @@ class ShutdownListener : ExecutionCondition {
     return if (shuttingDown) {
       val message = "Execution of '${context?.displayName}' cannot begin because shutdown is in progress, see AT-2253"
       logOutput(message)
+      AllureReport.reportFailure("BuildExecution",
+                                 "Test execution cannot begin because shutdown is in progress, probably Build timeout see AT-2253",
+                                 suffix = "BuildError",
+                                 originalStackTrace = "")
       ConditionEvaluationResult.disabled(message)
     }
     else {
