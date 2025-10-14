9a82353a4b8035d035585d42ad72e76f3ec611bf
Anna Koehler <anna.marchenko@jetbrains.com>
2025-10-14 19:18:45 +0200

    [AT-3001] Part one: use hash commits and not default branches in projects downloaded from the GitHub
    GitOrigin-RevId: cd70476797eb86bd643c282fc567c95b0947acf1

--- intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/IdeaUltimateCases.kt
+++ intellij.tools.plugin.checker.tests/testSrc/com/intellij/tools/plugin/checker/data/IdeaUltimateCases.kt
@@ -12,14 +12,18 @@ object IdeaUltimateCases : TestCaseTemplate(IdeProductProvider.IU) {
   )
 
   val MavenSimpleApp = withProject(
-    RemoteArchiveProjectInfo(
-      projectURL = "https://github.com/jenkins-docs/simple-java-maven-app/archive/refs/heads/master.zip",
+    GitHubProject.fromGithub(
+      branchName = "master",
+      commitHash = "428464cd6d3cee7ae7f139395c25b5f719bc3c94",
+      repoRelativeUrl = "jenkins-docs/simple-java-maven-app",
     )
   )
 
   val IntelliJCommunityProject = withProject(
-    RemoteArchiveProjectInfo(
-      projectURL = "https://github.com/JetBrains/intellij-community/archive/master.zip",
+    GitHubProject.fromGithub(
+      branchName = "master",
+      commitHash = "d0041ec5afd967d5129f360cfe92652d56a03024",
+      repoRelativeUrl = "JetBrains/intellij-community",
     )
   )
 }
