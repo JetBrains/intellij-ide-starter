6980dbb22bd9453c2d4aba57464e5af43c02d91b
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-10-14 16:09:51 +0200

    [starter] KillOutdatedProcessesAfterAll -> KillOutdatedProcesses
    Also kill all hanging all running test processes in before All without exceptions
    
    GitOrigin-RevId: 34b3c3c14aee2fd3d7b7fd68a2750263a3f46963

--- intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/config/KillOutdatedProcesses.kt
+++ intellij.tools.ide.starter.junit5/src/com/intellij/ide/starter/junit5/config/KillOutdatedProcesses.kt
@@ -2,10 +2,14 @@ package com.intellij.ide.starter.junit5.config
 
 import com.intellij.ide.starter.process.killOutdatedProcesses
 import org.junit.jupiter.api.extension.AfterAllCallback
-import org.junit.jupiter.api.extension.BeforeEachCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
-open class KillOutdatedProcessesAfterAll : AfterAllCallback {
+open class KillOutdatedProcesses : BeforeAllCallback, AfterAllCallback {
+  override fun beforeAll(context: ExtensionContext) {
+    killOutdatedProcesses()
+  }
+  
   override fun afterAll(context: ExtensionContext) {
     killOutdatedProcesses(reportErrors = true)
   }
