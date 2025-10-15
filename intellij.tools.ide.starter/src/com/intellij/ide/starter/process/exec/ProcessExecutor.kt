7c4598ff9b15c0d2bd82d49f106e713823efbffd
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2025-10-15 20:40:16 +0200

    [starter] ProcessExecutor.kt: use perClassSupervisorScope for onProcessCreatedJob as IDE can be shared across different test methods
    (cherry picked from commit 0e697f70c4e92637ab4f5ac04be507a7cb0f51ca)
    
    GitOrigin-RevId: 8b9c0d1956e43a6c342e40c881590124610628aa

--- intellij.tools.ide.starter/src/com/intellij/ide/starter/process/exec/ProcessExecutor.kt
+++ intellij.tools.ide.starter/src/com/intellij/ide/starter/process/exec/ProcessExecutor.kt
@@ -2,7 +2,7 @@ package com.intellij.ide.starter.process.exec
 
 import com.intellij.ide.starter.config.ConfigurationStorage
 import com.intellij.ide.starter.config.logEnvVariables
-import com.intellij.ide.starter.coroutine.perTestSupervisorScope
+import com.intellij.ide.starter.coroutine.perClassSupervisorScope
 import com.intellij.ide.starter.utils.catchAll
 import com.intellij.ide.starter.utils.getThrowableText
 import com.intellij.tools.ide.util.common.logError
@@ -193,7 +193,7 @@ class ProcessExecutor(
     @Suppress("BlockingMethodInNonBlockingContext") val process = processBuilder.start()
 
     val processId = process.pid()
-    val onProcessCreatedJob: Job = perTestSupervisorScope.launch(Dispatchers.IO) {
+    val onProcessCreatedJob: Job = perClassSupervisorScope.launch(Dispatchers.IO) {
       if (!silent) logOutput("  ... started external process `$presentableName` with process ID = $processId")
       onProcessCreated(process, processId)
     }
