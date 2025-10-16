d106debcc1d2a40dd6d5a071fe73896b8176d228
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2025-10-16 18:48:56 +0300

    [split tests] AT-3274 Moving intellij.tools.ide.starter.driver to community
    GitOrigin-RevId: a61c0e5bff64e03464def323618f45a0ae7fa9aa

--- intellij.tools.ide.starter.driver/src/com/intellij/ide/starter/driver/PlaybackRunnerService.kt
+++
@@ -1,9 +0,0 @@
-package com.intellij.ide.starter.driver
-
-import com.intellij.driver.client.Remote
-import com.intellij.driver.sdk.Project
-
-@Remote("com.jetbrains.performancePlugin.PlaybackRunnerService", plugin = "com.jetbrains.performancePlugin")
-interface PlaybackRunnerService {
-  fun runScript(project: Project, script: String)
-}
