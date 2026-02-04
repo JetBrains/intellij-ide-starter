0262dbfd0782bb8546d18c793cbfff23317cfabe
Anastasia Katsman <anastasia.katsman@jetbrains.com>
2026-02-04 15:07:56 +0100

    testFramework [starter-driver]: bring back testing of both modes in UiTestWithDriver
    GitOrigin-RevId: 1d24534f15cab761c31b498d0b0b7e91b1be7ffa

--- intellij.tools.ide.starter.examples/testSrc/com/intellij/ide/starter/examples/driver/UiTestWithDriver.kt
+++ intellij.tools.ide.starter.examples/testSrc/com/intellij/ide/starter/examples/driver/UiTestWithDriver.kt
@@ -35,7 +35,7 @@ class UiTestWithDriver {
    * with IDE backend and frontend running on the same host.
    */
   @ParameterizedTest(name = "split-mode={0}")
-  @ValueSource(booleans = [true])
+  @ValueSource(booleans = [false, true])
   fun openEditorFromStructureViewEnterCommentLine(splitMode: Boolean) {
     if (splitMode) {
       di = DI {
